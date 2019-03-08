b575ea6d374a20cf0cfec5024a1f2de4aab48180
Liam Dawson <liamrdawson@gmail.com>
2019-03-08 19:52:40 +0000

    change code in index.html and flickr.js to allow for pictures to show from search result

--- js/flickr.js
+++ js/flickr.js
@@ -1,27 +1,29 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $('button').click(function () {
-        $('button').removeClass("selected");
-        $(this).addClass("selected");
-        let flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-        let photographer = $(this).text();
-        let flickrOptions = { 
-            tags: photographer,
-            format: "json"
-        };
-        function displayPhotos(data) {
-            let photoHTML = '<ul>';
-            $.each( data.items, function (i, photo) {
-                photoHTML += '<li class="grid-25 tablet-grid-50">';
-                photoHTML += '<a href="' + photo.link + '" class="image">';
-                photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-                
-            });
-            photoHTML += '</ul>';
-            $('#photos').html(photoHTML);
-        }
-        $.getJSON(flickrAPI, flickrOptions, displayPhotos);
-    }); 
 
-}); // END READY
+    $('form').submit(function (event) {
+       event.preventDefault();
+   
+       var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+       var searchTerm = $('#search').val();
+       var flickrOptions = {
+         tags: searchTerm,
+         format: "json"
+       };
+
+       function displayPhotos(data) {
+         var photoHTML = '<ul>';
+         $.each(data.items,function(i,photo) {
+           photoHTML += '<li class="grid-25 tablet-grid-50">';
+           photoHTML += '<a href="' + photo.link + '" class="image">';
+           photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+         }); // end each
+         photoHTML += '</ul>';
+         $('#photos').html(photoHTML);
+       }
+       $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+   
+     }); // end click
+   
+   }); // end ready
 
