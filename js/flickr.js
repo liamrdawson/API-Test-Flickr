cec7ffa8b4361fad1b500fff873473ce87dda2c8
Liam Dawson <liamrdawson@gmail.com>
2019-03-08 20:03:04 +0000

    change status of search button and disabled search field during search load

--- js/flickr.js
+++ js/flickr.js
@@ -2,26 +2,32 @@ $(document).ready(function() {
 
 
     $('form').submit(function (event) {
-       event.preventDefault();
-   
-       var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-       var searchTerm = $('#search').val();
-       var flickrOptions = {
-         tags: searchTerm,
-         format: "json"
-       };
+        event.preventDefault();
+        var $searchField = $('#search');
+        var $submitButton = $('#submit');
 
-       function displayPhotos(data) {
-         var photoHTML = '<ul>';
-         $.each(data.items,function(i,photo) {
-           photoHTML += '<li class="grid-25 tablet-grid-50">';
-           photoHTML += '<a href="' + photo.link + '" class="image">';
-           photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-         }); // end each
-         photoHTML += '</ul>';
-         $('#photos').html(photoHTML);
-       }
-       $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+        $searchField.prop("disabled", true);
+        $submitButton.attr("disabled", true).val("searching...");
+        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        var searchTerm = $searchField.val();
+        var flickrOptions = {
+            tags: searchTerm,
+            format: "json"
+        };
+
+        function displayPhotos(data) {
+            var photoHTML = '<ul>';
+            $.each(data.items,function(i,photo) {
+            photoHTML += '<li class="grid-25 tablet-grid-50">';
+            photoHTML += '<a href="' + photo.link + '" class="image">';
+            photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+            }); // end each
+            photoHTML += '</ul>';
+            $('#photos').html(photoHTML);
+            $searchField.prop("disabled", false);
+            $submitButton.attr("disabled", false).val("Search");
+        }
+        $.getJSON(flickerAPI, flickrOptions, displayPhotos);
    
      }); // end click
    
